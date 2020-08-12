24e3efc2c5f8a6da8ab364bda1c1e2fb0f7c4790
Yumo Xu <kevin.xym@outlook.com>
2020-08-12 21:58:11 +0100

    commit

--- s2s-ft/s2s_ft/modeling_qfs_decoding.py
+++ s2s-ft/s2s_ft/modeling_qfs_decoding.py
@@ -1020,7 +1020,7 @@ class MargeDiscriminator(nn.Module):
 
     def get_loss(self, pred):
         pred = pred.view(-1)
-        label = torch.tensor(pred.shape[0] * [self.label], device=self.device, dtype=torch.half)
+        label = torch.tensor(pred.shape[0] * [self.label], device=self.device, dtype=torch.float)
         # print(f'label: {label.size()}, pred: {pred.size()}')
         loss = MSELoss()(pred, label)
         return loss
