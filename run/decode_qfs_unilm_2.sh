ea28c3b281d8f94b35dfea4dbe4f2ab7d20af12f
Yumo Xu <kevin.xym@outlook.com>
2020-08-16 01:23:41 +0100

    commit

--- run/decode_qfs_unilm_2.sh
+++ run/decode_qfs_unilm_2.sh
@@ -1,7 +1,7 @@
 export PROJ_PATH=/disk/nfs/ostrom/s1617290/unilm
 
-export INPUT_FILE_NAME=marge-13_config-37500_iter-narr-ir-dial-tf-2005-global_pos
-export SPLIT=${INPUT_FILE_NAME}-no_min
+export INPUT_FILE_NAME=marge-13_config-37500_iter-narr-ir-dial-tf-2005-top90-global_pos
+export SPLIT=${INPUT_FILE_NAME}
 export MODEL_PATH=$PROJ_PATH/model/unilm_2
 export QFS_PROJ_ROOT=/disk/nfs/ostrom/s1617290/shiftsum
 export INPUT_JSON=${QFS_PROJ_ROOT}/unilm_in/unilm_in-${INPUT_FILE_NAME}.json
@@ -22,12 +22,12 @@ $python $python_file \
   --split ${SPLIT} \
   --model_path ${MODEL_PATH} \
   --model_ckpt ${CKPT} \
-  --max_seq_length 812 \
-  --max_tgt_length 300 \
+  --max_seq_length 1168 \
+  --max_tgt_length 400 \
   --batch_size 32 \
   --beam_size 5 \
   --length_penalty 0.9 \
   --forbid_duplicate_ngrams \
   --mode s2s \
   --forbid_ignore_word "." \
-  # --min_len 300
+  --min_len 300
