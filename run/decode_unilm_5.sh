fdb4f8fe248eac5c856a5d21604cb0baf7731922
Yumo Xu <kevin.xym@outlook.com>
2020-08-22 20:31:55 +0100

    commit

--- run/decode_unilm_5.sh
+++ run/decode_unilm_5.sh
@@ -4,11 +4,11 @@ export MODEL_PATH=$PROJ_PATH/model/unilm_5
 export SPLIT=val
 export INPUT_JSON=$PROJ_PATH/data/multinews-250-gold_rank/${SPLIT}.json
 
-export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+export CUDA_VISIBLE_DEVICES=0,1,2,3
 export OMP_NUM_THREADS=4
 export MKL_NUM_THREADS=4
 
-export CKPT=10500,12000,13500,15000,16500,18000,19500,21000
+export CKPT=22500,24000
 export python=$PROJ_PATH/bin/python
 export python_file=$PROJ_PATH/s2s-ft/decode_seq2seq.py
 
@@ -22,7 +22,7 @@ $python $python_file \
   --model_ckpt ${CKPT} \
   --max_seq_length 768 \
   --max_tgt_length 400 \
-  --batch_size 256 \
+  --batch_size 128 \
   --beam_size 5 \
   --length_penalty 0 \
   --forbid_duplicate_ngrams \
