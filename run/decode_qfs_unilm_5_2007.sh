d7dbf3e6e63ab3320d0e7e6fd0fd22efee21adca
Yumo Xu <kevin.xym@outlook.com>
2020-08-25 02:27:00 +0100

    commit

--- run/decode_qfs_unilm_5_2007.sh
+++ run/decode_qfs_unilm_5_2007.sh
@@ -1,6 +1,6 @@
 export PROJ_PATH=/disk/nfs/ostrom/s1617290/unilm
 
-export INPUT_FILE_NAME=centrality-hard_bias-0.85_damp-rr_records-rr-0_config-45000_iter-query-ir-dial-tf-2006-90_qa_topK-0.6_cos-4_wan
+export INPUT_FILE_NAME=centrality-hard_bias-0.85_damp-rr_records-rr-0_config-45000_iter-query-ir-dial-tf-2005-90_qa_topK-0.6_cos-4_wan
 export SPLIT=${INPUT_FILE_NAME}
 export MODEL_PATH=$PROJ_PATH/model/unilm_5
 export QFS_PROJ_ROOT=/disk/nfs/ostrom/s1617290/shiftsum
