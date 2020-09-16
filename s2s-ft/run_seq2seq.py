0fa0d94425f99032e36942784d5a395cf9c978b1
Yumo Xu <kevin.xym@outlook.com>
2020-09-17 00:45:46 +0100

    commit

--- s2s-ft/run_seq2seq.py
+++ s2s-ft/run_seq2seq.py
@@ -382,7 +382,7 @@ def get_model_and_tokenizer(args):
 
     logger.info("Model config for seq2seq: %s", str(config))
 
-    if arg.prepend_len:
+    if args.prepend_len:
         tgt_segments = [85] + list(range(100, 400, 15)) + [400]
         additional_special_tokens = [f'[unused{seg}]' for seg in tgt_segments]
         logger.info('additional_special_tokens: {additional_special_tokens}')
