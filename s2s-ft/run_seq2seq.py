b4d884fcf302de37cdbc8cc03309060db254de15
Yumo Xu <kevin.xym@outlook.com>
2020-09-19 00:06:00 +0100

    commit

--- s2s-ft/run_seq2seq.py
+++ s2s-ft/run_seq2seq.py
@@ -386,14 +386,14 @@ def get_model_and_tokenizer(args):
         tgt_segments = [85] + list(range(100, 400, 15)) + [400]
         additional_special_tokens = [f'[unused{seg}]' for seg in tgt_segments]
         logger.info(f'additional_special_tokens: {additional_special_tokens}')
-        tokenizer = TOKENIZER_CLASSES[args.model_type].from_pretrained(
-            args.tokenizer_name, do_lower_case=args.do_lower_case, 
-            cache_dir=args.cache_dir if args.cache_dir else None,
+        tokenizer = tokenizer_class.from_pretrained(
+            args.tokenizer_name if args.tokenizer_name else args.model_name_or_path,
+            do_lower_case=args.do_lower_case, cache_dir=args.cache_dir if args.cache_dir else None,
             additional_special_tokens=additional_special_tokens)
     else:
-        tokenizer = TOKENIZER_CLASSES[args.model_type].from_pretrained(
-            args.tokenizer_name, do_lower_case=args.do_lower_case, 
-            cache_dir=args.cache_dir if args.cache_dir else None)
+        tokenizer = tokenizer_class.from_pretrained(
+            args.tokenizer_name if args.tokenizer_name else args.model_name_or_path,
+            do_lower_case=args.do_lower_case, cache_dir=args.cache_dir if args.cache_dir else None)
 
     model = BertForSequenceToSequence.from_pretrained(
         args.model_name_or_path, config=config, model_type=args.model_type,
