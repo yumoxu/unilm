a9af507d5c7619026d8c8058626940494f9c4e09
Yumo Xu <kevin.xym@outlook.com>
2020-09-19 23:16:02 +0100

    commit

--- run/decode_qfs_unilm_9.sh
+++ run/decode_qfs_unilm_9.sh
@@ -1,6 +1,6 @@
 export PROJ_PATH=/disk/nfs/ostrom/s1617290/unilm
 
-export INPUT_FILE_NAME=rr-34_config-25000_iter-query-ir-dial-tf-2006-top150
+export INPUT_FILE_NAME=rr-34_config-25000_iter-query-ir-dial-tf-2007-top150
 export SPLIT=${INPUT_FILE_NAME}
 export MODEL_PATH=$PROJ_PATH/model/unilm_9
 export QFS_PROJ_ROOT=/disk/nfs/ostrom/s1617290/shiftsum
