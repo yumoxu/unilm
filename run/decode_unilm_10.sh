bff5d0ed02dd783edf27a01e3c26bdfc054436bf
Yumo Xu <kevin.xym@outlook.com>
2020-09-20 11:39:51 +0100

    commit

--- run/decode_unilm_10.sh
+++ run/decode_unilm_10.sh
@@ -9,7 +9,7 @@ export OMP_NUM_THREADS=8
 export MKL_NUM_THREADS=8
 
 # 1500,3000,4500,6000,7500,9000,10500,12000,13500,15000,16500,18000,19500
-export CKPT=10500
+export CKPT=12000
 export python=$PROJ_PATH/bin/python
 export python_file=$PROJ_PATH/s2s-ft/decode_seq2seq.py
 
