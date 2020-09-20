6623a17191b1605351561de84b129e66eca06031
Yumo Xu <kevin.xym@outlook.com>
2020-09-20 20:22:06 +0100

    commit

---
+++ run/eval_unilm_10.sh
@@ -0,0 +1,15 @@
+export PROJ_PATH=/disk/nfs/ostrom/s1617290/unilm
+export SPLIT=val
+export GOLD_PATH=${PROJ_PATH}/data/multinews-gold_rank_recall_prepend_len/${SPLIT}.json
+export CKPT=*
+export PRED_PATH="${PROJ_PATH}/model/unilm_10/ckpt-${CKPT}.${SPLIT}"
+export python=$PROJ_PATH/bin/python
+export python_file=$PROJ_PATH/s2s-ft/evaluations/eval_for_multinews.py
+
+$python ${python_file} \
+    --pred "${PRED_PATH}" \
+    --gold ${GOLD_PATH} \
+    --split ${SPLIT} \
+    --save_best \
+    --processes 32 \
+    --trunc_len 300
