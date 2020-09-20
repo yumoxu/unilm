9d0fd1bf4ba44c90e57e019496896f18c0c95a9c
Yumo Xu <kevin.xym@outlook.com>
2020-09-20 12:08:38 +0100

    commit

--- run/decode_unilm_10.sh
+++ run/decode_unilm_10.sh
@@ -4,9 +4,9 @@ export MODEL_PATH=$PROJ_PATH/model/unilm_10
 export SPLIT=val
 export INPUT_JSON=$PROJ_PATH/data/multinews-gold_rank_recall_prepend_len/${SPLIT}.json
 
-export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
-export OMP_NUM_THREADS=8
-export MKL_NUM_THREADS=8
+export CUDA_VISIBLE_DEVICES=0,1,2,3
+export OMP_NUM_THREADS=4
+export MKL_NUM_THREADS=4
 
 # 1500,3000,4500,6000,7500,9000,10500,12000,13500,15000,16500,18000,19500
 export CKPT=13500
@@ -24,7 +24,7 @@ $python $python_file \
   --model_ckpt ${CKPT} \
   --max_seq_length 1068 \
   --max_tgt_length 300 \
-  --batch_size 192 \
+  --batch_size 96 \
   --beam_size 5 \
   --length_penalty 0 \
   --forbid_duplicate_ngrams \
