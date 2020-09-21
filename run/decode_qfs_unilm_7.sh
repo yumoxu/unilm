d887e69aad34ae8beeadf8a4ed2c1765c6cf36c6
Yumo Xu <kevin.xym@outlook.com>
2020-09-21 12:54:54 +0100

    commit

--- run/decode_qfs_unilm_7.sh
+++ run/decode_qfs_unilm_7.sh
@@ -1,8 +1,8 @@
 export PROJ_PATH=/disk/nfs/ostrom/s1617290/unilm
 
 # RR: rr-34_config-25000_iter-query-ir-dial-tf-2006-top150-prepend_len
-# QA: unilm_in-qa-bert-narr-ir-tf-2006-top90-prepend_len.json
-export INPUT_FILE_NAME=unilm_in-qa-bert-narr-ir-tf-2006-top90-prepend_len.json
+# QA: unilm_in-qa-bert-narr-ir-tf-2006-top90-prepend_len
+export INPUT_FILE_NAME=unilm_in-qa-bert-narr-ir-tf-2006-top90-prepend_len
 export SPLIT=${INPUT_FILE_NAME}
 export MODEL_PATH=$PROJ_PATH/model/unilm_7
 export QFS_PROJ_ROOT=/disk/nfs/ostrom/s1617290/shiftsum
