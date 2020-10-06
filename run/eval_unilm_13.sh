96a79d352c8d37e944f3378c70ed32b383173121
Yumo Xu <kevin.xym@outlook.com>
2020-10-06 20:59:44 +0100

    commit

--- run/eval_unilm_13.sh
+++ run/eval_unilm_13.sh
@@ -2,7 +2,7 @@ export PROJ_PATH=/disk/nfs/ostrom/s1617290/unilm
 export SPLIT=val
 
 export GOLD_PATH=${PROJ_PATH}/data/rouge_estimated-rr-34_config-25000_iter-masked_summ-mn-marge_cluster-ratio-reveal_0.0_prepend_len/${SPLIT}.json
-export CKPT=27000
+export CKPT=28500
 export PRED_PATH="${PROJ_PATH}/model/unilm_13/ckpt-${CKPT}.${SPLIT}"
 export python=$PROJ_PATH/bin/python
 export python_file=$PROJ_PATH/s2s-ft/evaluations/eval_for_multinews.py
