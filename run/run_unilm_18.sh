536beeb7bcf37aab2045ea0bd9699c55b89fbaf6
Yumo Xu <kevin.xym@outlook.com>
2020-11-25 13:30:41 +0000

    commit

--- run/run_unilm_18.sh
+++ run/run_unilm_18.sh
@@ -1,8 +1,8 @@
-export MODEL_NAME=unilm_17
+export MODEL_NAME=unilm_18
 
 export PROJ_ROOT=/disk/nfs/ostrom/s1617290/unilm
 
-export DATA_DIR=${PROJ_ROOT}/data/multinews-gold_rank_f1_prepend_len_prepend_q
+export DATA_DIR=${PROJ_ROOT}/data/cnndm-gold_rank_f1_prepend_len_prepend_q
 export TRAIN_FILE=${DATA_DIR}/train.json
 
 export OUTPUT_DIR=${PROJ_ROOT}/model/${MODEL_NAME}
