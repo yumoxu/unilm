df905f8ad44576af4dc357b984c03dc6315e9022
Yumo Xu <kevin.xym@outlook.com>
2020-11-26 16:20:01 +0000

    commit

--- run/decode_qfs_unilm_18.sh
+++ run/decode_qfs_unilm_18.sh
@@ -19,6 +19,7 @@ export PROJ_PATH=/disk/nfs/ostrom/s1617290/unilm
 
 # rr-34_config-25000_iter-pred@grsum-tdqfs-0.6_cos-0_wan-nw_250@masked-ratio-reveal_1.0-ir-dial-tf-tdqfs-top150-prepend_len-prepend_add_left_mask_right_dot_q
 export INPUT_FILE_NAME=rr-34_config-25000_iter-query-ir-dial-tf-2007-top150-prepend_len-prepend_masked_q
+export SPLIT=${INPUT_FILE_NAME}
 export MODEL_PATH=$PROJ_PATH/model/unilm_18
 export QFS_PROJ_ROOT=/disk/nfs/ostrom/s1617290/shiftsum
 export INPUT_JSON=${QFS_PROJ_ROOT}/unilm_in/unilm_in-${INPUT_FILE_NAME}.json
