2d55dd2e7d4ef44b6fc33fd734ac9fe0f544c1ec
Yumo Xu <kevin.xym@outlook.com>
2020-12-03 15:55:34 +0000

    commit

--- run/run_unilm_20.sh
+++ run/run_unilm_20.sh
@@ -12,7 +12,6 @@ export CACHE_DIR=${PROJ_ROOT}/model/cache_unilm
 export python=${PROJ_ROOT}/bin/python
 export python_file=${PROJ_ROOT}/s2s-ft/run_seq2seq.py
 
-# export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 export CUDA_VISIBLE_DEVICES=0,1,2,3
 $python -m torch.distributed.launch --nproc_per_node=8 ${python_file} \
   --train_file $TRAIN_FILE \
