b26b4bad0b2dbf12b52082206fbaae0bd0316542
Yumo Xu <kevin.xym@outlook.com>
2020-12-04 17:10:02 +0000

    commit

--- run/run_unilm_20.sh
+++ run/run_unilm_20.sh
@@ -1,3 +1,4 @@
+#### This script is for: UniLM-CNN/DM
 export MODEL_NAME=unilm_20
 
 export PROJ_ROOT=/disk/nfs/ostrom/s1617290/unilm
@@ -19,7 +20,6 @@ $python -m torch.distributed.launch --nproc_per_node=4 ${python_file} \
   --model_type unilm \
   --model_name_or_path $MODEL_DIR \
   --do_lower_case \
-  --prepend_len \
   --fp16 \
   --fp16_opt_level O2 \
   --max_source_seq_length 768 \
@@ -28,6 +28,6 @@ $python -m torch.distributed.launch --nproc_per_node=4 ${python_file} \
   --gradient_accumulation_steps 8 \
   --learning_rate 7e-5 \
   --num_warmup_steps 1000 \
-  --num_training_steps 10000 \
+  --num_training_steps 45000 \
   --cache_dir $CACHE_DIR \
-  --save_steps 500
+  --save_steps 1500
