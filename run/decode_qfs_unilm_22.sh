e0f1b8c32c7fc86a77193657bd6b02d57f7135fb
Yumo Xu <kevin.xym@outlook.com>
2020-12-05 01:04:33 +0000

    commit

--- run/decode_qfs_unilm_22.sh
+++ run/decode_qfs_unilm_22.sh
@@ -20,7 +20,7 @@ export PROJ_PATH=/disk/nfs/ostrom/s1617290/unilm
 # rr-39_config-26000_iter-add_left_mask_right_dot-ir-dial-tf-tdqfs-0.6_cos-nw_250-prepend_len-prepend_masked_q
 # rr-34_config-25000_iter-pred@grsum-tdqfs-0.6_cos-0_wan-nw_250@masked-ratio-reveal_1.0-ir-dial-tf-tdqfs-top150-prepend_len-prepend_masked_q
 # rr-34_config-25000_iter-pred@grsum-tdqfs-0.6_cos-0_wan-nw_250@masked-ratio-reveal_1.0-ir-dial-tf-tdqfs-top150-prepend_len-prepend_add_left_mask_right_dot_q
-export INPUT_FILE_NAME=rr-34_config-25000_iter-query-ir-dial-tf-2007-top150-prepend_len
+export INPUT_FILE_NAME=rr-34_config-25000_iter-query-ir-dial-tf-2006-top150-prepend_len
 export SPLIT=${INPUT_FILE_NAME}
 export MODEL_PATH=$PROJ_PATH/model/unilm_22
 export QFS_PROJ_ROOT=/disk/nfs/ostrom/s1617290/shiftsum
@@ -32,7 +32,7 @@ export MKL_NUM_THREADS=4
 
 # 500,1000,1500,2000,2500,3000,3500,4000,4500
 # 5000,5500,6000,6500,7000,7500,8000,8500,9000, 9500,10000
-export CKPT=5000,5500,6000,6500,7000
+export CKPT=5000
 export python=$PROJ_PATH/bin/python
 export python_file=$PROJ_PATH/s2s-ft/decode_seq2seq.py
 
