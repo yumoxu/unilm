f4312601ec4455b6f50c4e07208055000ff68944
Yumo Xu <kevin.xym@outlook.com>
2020-12-07 01:18:53 +0000

    commit

--- run/decode_qfs_unilm_17.sh
+++ run/decode_qfs_unilm_17.sh
@@ -2,9 +2,7 @@ export PROJ_PATH=/disk/nfs/ostrom/s1617290/unilm
 
 # RR-SUBQ: rr_subq-narr-rr_records-rr-34_config-25000_iter-query-ir-dial-tf-2007-150_qa_topK-prepend_len
 # RR: rr-34_config-25000_iter-query-ir-dial-tf-2006-top150-prepend_len
-# RR-0.6: rr-34_config-25000_iter-query-ir-dial-tf-2006-0.6_cos-prepend_len
-# QA: qa-bert-narr-ir-tf-2006-top90-prepend_len
-# IR: ir-tf-2006-conf0.75-prepend_len
+# QA: qa-bert-narr-ir-dial-tf-2007-top90-prepend_len-prepend_masked_q
 
 # RR-with query: 
 # rr-34_config-25000_iter-query-ir-dial-tf-2007-top150-prepend_len-prepend_raw_q
@@ -20,7 +18,7 @@ export PROJ_PATH=/disk/nfs/ostrom/s1617290/unilm
 # rr-39_config-26000_iter-add_left_mask_right_dot-ir-dial-tf-tdqfs-0.6_cos-nw_250-prepend_len-prepend_masked_q
 # rr-34_config-25000_iter-pred@grsum-tdqfs-0.6_cos-0_wan-nw_250@masked-ratio-reveal_1.0-ir-dial-tf-tdqfs-top150-prepend_len-prepend_masked_q
 # rr-34_config-25000_iter-pred@grsum-tdqfs-0.6_cos-0_wan-nw_250@masked-ratio-reveal_1.0-ir-dial-tf-tdqfs-top150-prepend_len-prepend_add_left_mask_right_dot_q
-export INPUT_FILE_NAME=rr-39_config-26000_iter-query-ir-dial-tf-2007-top150-prepend_len-prepend_masked_q
+export INPUT_FILE_NAME=qa-bert-narr-ir-dial-tf-2007-top90-prepend_len-prepend_masked_q
 export SPLIT=${INPUT_FILE_NAME}
 export MODEL_PATH=$PROJ_PATH/model/unilm_17
 export QFS_PROJ_ROOT=/disk/nfs/ostrom/s1617290/shiftsum
@@ -32,7 +30,7 @@ export MKL_NUM_THREADS=4
 
 # 500,1000,1500,2000,2500,3000,3500,4000,4500
 # 5000,5500,6000,6500,7000,7500,8000,8500,9000, 9500,10000
-export CKPT=1000,1500,2000,2500,3000,3500,4000,4500,5000
+export CKPT=2500
 export python=$PROJ_PATH/bin/python
 export python_file=$PROJ_PATH/s2s-ft/decode_seq2seq.py
 
