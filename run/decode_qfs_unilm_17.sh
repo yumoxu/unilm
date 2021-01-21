15e5b8bd34a75432b83b33a1c57d50130a208e30
Yumo Xu <kevin.xym@outlook.com>
2021-01-21 23:40:43 +0000

    commit

--- run/decode_qfs_unilm_17.sh
+++ run/decode_qfs_unilm_17.sh
@@ -4,6 +4,7 @@ export PROJ_PATH=/disk/nfs/ostrom/s1617290/unilm
 # RR: rr-34_config-25000_iter-query-ir-dial-tf-2006-top150-prepend_len
 # QA: qa-bert-narr-ir-dial-tf-2007-top90-prepend_len-prepend_masked_q
 # QA-tdqfs: qa-bert-narr-ir-dial-tf-tdqfs-top90-prepend_len-prepend_add_left_mask_right_dot_q
+# QA-DUC: qa-bert-narr-ir-dial-tf-2006-top90-prepend_len-prepend_masked_q
 
 # RR-with query: 
 # rr-34_config-25000_iter-query-ir-dial-tf-2007-top150-prepend_len-prepend_raw_q
@@ -19,7 +20,7 @@ export PROJ_PATH=/disk/nfs/ostrom/s1617290/unilm
 # rr-39_config-26000_iter-add_left_mask_right_dot-ir-dial-tf-tdqfs-0.6_cos-nw_250-prepend_len-prepend_masked_q
 # rr-34_config-25000_iter-pred@grsum-tdqfs-0.6_cos-0_wan-nw_250@masked-ratio-reveal_1.0-ir-dial-tf-tdqfs-top150-prepend_len-prepend_masked_q
 # rr-34_config-25000_iter-pred@grsum-tdqfs-0.6_cos-0_wan-nw_250@masked-ratio-reveal_1.0-ir-dial-tf-tdqfs-top150-prepend_len-prepend_add_left_mask_right_dot_q
-export INPUT_FILE_NAME=qa-bert-narr-ir-dial-tf-tdqfs-top90-prepend_len-prepend_add_left_mask_right_dot_q
+export INPUT_FILE_NAME=qa-bert-narr-ir-dial-tf-2006-top90-prepend_len-prepend_masked_q
 export SPLIT=${INPUT_FILE_NAME}
 export MODEL_PATH=$PROJ_PATH/model/unilm_17
 export QFS_PROJ_ROOT=/disk/nfs/ostrom/s1617290/shiftsum
